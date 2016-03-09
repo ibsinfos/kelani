7e917974ad98793dd7214a85a7563fc79873fe79
nishan <nishan4r@gmail.com>
2016-03-09 19:12:09 +0530

    added privilages

--- userPrivileges.php
+++ userPrivileges.php
@@ -11,63 +11,49 @@
     <meta name="author" content="">
 
 
+    <!-- Bootstrap Core CSS -->
 
-<!-- Bootstrap Core CSS -->
+    <link href="./css/bootstrap.min.css" rel="stylesheet">
 
-<link href="./css/bootstrap.min.css" rel="stylesheet">
+    <!-- Custom CSS -->
+    <link href="./css/sb-admin.css" rel="stylesheet">
 
-<!-- Custom CSS -->
-<link href="./css/sb-admin.css" rel="stylesheet">
+    <!-- Morris Charts CSS -->
+    <link href="./css/plugins/morris.css" rel="stylesheet">
 
-<!-- Morris Charts CSS -->
-<link href="./css/plugins/morris.css" rel="stylesheet">
+    <!-- Custom Fonts -->
+    <link href="./font-awesome/css/font-awesome.min.css" rel="stylesheet" type="text/css">
 
-<!-- Custom Fonts -->
-<link href="./font-awesome/css/font-awesome.min.css" rel="stylesheet" type="text/css">
+    <!-- Custom Css -->
+    <link href="./css/formcss.css" rel="stylesheet" type="text/css">
 
-<!-- Custom Css -->
-<link href="./css/formcss.css" rel="stylesheet" type="text/css">
-
-<!-- HTML5 Shim and Respond.js IE8 support of HTML5 elements and media queries -->
-<!-- WARNING: Respond.js doesn't work if you view the page via file:// -->
-<!--[if lt IE 9]>
-        <script src="https://oss.maxcdn.com/libs/html5shiv/3.7.0/html5shiv.js"></script>
-        <script src="https://oss.maxcdn.com/libs/respond.js/1.4.2/respond.min.js"></script>
+    <!-- HTML5 Shim and Respond.js IE8 support of HTML5 elements and media queries -->
+    <!-- WARNING: Respond.js doesn't work if you view the page via file:// -->
+    <!--[if lt IE 9]>
+    <script src="https://oss.maxcdn.com/libs/html5shiv/3.7.0/html5shiv.js"></script>
+    <script src="https://oss.maxcdn.com/libs/respond.js/1.4.2/respond.min.js"></script>
     <![endif]-->
 
-<script type="text/javascript">
+    <script type="text/javascript">
 
-</script>  
-    
+    </script>
+
 </head>
 <body>
 
 <?php
 include_once 'dbconfig.php'; //Connect to database
-//include_once './inc/top.php';
+include_once './inc/top.php';
 
 $row_edt = null;
-if(isset($_GET['edit'])){
+if (isset($_GET['edit'])) {
     $id = trim($_GET['edit']);
-    $query = "SELECT p.UserLevel_tbl_id, p.Form_tbl_FormID, f.Name, p.R, p.W, p.D FROM privileges_tbl p, form_tbl f WHERE p.UserLevel_tbl_id=".$id." and f.FormID = p.Form_tbl_FormID";
+    $query = "SELECT p.UserLevel_tbl_id, p.Form_tbl_FormID, f.Name, p.R, p.W, p.D FROM privileges_tbl p, form_tbl f WHERE p.UserLevel_tbl_id=" . $id . " and f.FormID = p.Form_tbl_FormID";
 
     $result = getData($query);
     if (mysqli_num_rows($result) > 0) {
         $row_edt = mysqli_fetch_all($result);
-//        var_dump($row_edt); exit();
-//        while ($row_edt = mysqli_fetch_assoc($result)) {
-//            $userlevel_ = $row_edt['UserLevel_tbl_id'];
-//            $form_ = $row_edt['Form_tbl_FormID'];
-//            $cbr_ = $row_edt['R'];
-//            $cbw_ = $row_edt['W'];
-//            $cbd_ = $row_edt['D'];
-//
-//            $btnStatus = 'enabled';
-//            $btnAddStatus = 'disabled';
-//        }
-//        exit();
-    }
-    else{
+    } else {
         $userlevel_ = "";
         $form_ = "";
         $cbr_ = "";
@@ -77,8 +63,7 @@ if(isset($_GET['edit'])){
         $btnStatus = 'disabled';
         $btnAddStatus = 'enabled';
     }
-}
-else{
+} else {
     $userlevel_ = "";
     $form_ = "";
     $cbr_ = "";
@@ -91,43 +76,45 @@ else{
 ?>
 ?>
 
-    <div id="wrapper">
-        <div id="page-wrapper">
+<div id="wrapper">
+    <div id="page-wrapper">
 
-            <div class="container-fluid">
+        <div class="container-fluid">
 
-                <!-- Page Heading -->
-                <div class="row">
-                    <div class="col-lg-12">
-                        <h1 class="page-header">
-                            User Privileges
-                        </h1>
-                        <ol class="breadcrumb">
-                            <li>
-                                <i class="fa fa-dashboard"></i>  <a href="index.php">Dashboard</a>
-                            </li>
-                            <li class="active">
-                                <i class="fa fa-bar-chart-o"></i> User Privileges
-                            </li>
-                        </ol>
-                    </div>
+            <!-- Page Heading -->
+            <div class="row">
+                <div class="col-lg-12">
+                    <h1 class="page-header">
+                        User Privileges
+                    </h1>
+                    <ol class="breadcrumb">
+                        <li>
+                            <i class="fa fa-dashboard"></i> <a href="index.php">Dashboard</a>
+                        </li>
+                        <li class="active">
+                            <i class="fa fa-bar-chart-o"></i> User Privileges
+                        </li>
+                    </ol>
                 </div>
-                <!-- /.row -->
-                <div class="row">
-                    <?php
-                    require_once("./config.php");
-                    $stmt = $db_con->prepare("SELECT * FROM privileges_tbl WHERE UserLevel_tbl_id = '" . $_SESSION['userLvl'] . "' AND Form_tbl_FormID = 'usrpri'");
-                    $stmt->execute();
-                    $permissions = $stmt->fetchAll();
-                    if($permissions[0]['R']){?>
-                    <form method="post" action="controller/userPrivilegesController.php" target="_self" data-toggle="validator" id="usrpri">
-                        <div class="col-lg-6">
+            </div>
+            <!-- /.row -->
+            <div class="row">
+                <?php
+                require_once("./config.php");
+                $stmt = $db_con->prepare("SELECT * FROM privileges_tbl WHERE UserLevel_tbl_id = '" . $_SESSION['userLvl'] . "' AND Form_tbl_FormID = 'usrpri'");
+                $stmt->execute();
+                $permissions = $stmt->fetchAll();
+                if ($permissions[0]['R']){
+                ?>
+                <form method="post" action="controller/userPrivilegesController.php" target="_self"
+                      data-toggle="validator" id="usrpri">
+                    <div class="col-lg-6">
 
                         <div class="form-group">
-                        <label class="control-label col-md-4">User Level</label><br/>
-                        <select class="form-control col-md-8" name="cmbUserLevel" id="cmbUserLevel">
-                        <option value='0'>        --Select UserLevel--</option>
-                        		<?php
+                            <label class="control-label col-md-4">User Level</label><br/>
+                            <select class="form-control col-md-8" name="cmbUserLevel" id="cmbUserLevel">
+                                <option value='0'> --Select UserLevel--</option>
+                                <?php
                                 include_once 'dbconfig.php';
                                 $query = 'SELECT * FROM userlevel_tbl;';
                                 $result = getData($query);
@@ -135,119 +122,127 @@ else{
                                     // output data of each row
                                     while ($row = mysqli_fetch_assoc($result)) {
                                         $selected = $row['id'] == $row_edt[0][0] ? 'selected' : '';
-                                        echo "<option ". $selected ." value='".$row['id']."'>".$row['lavel_name']."</option>";
+                                        echo "<option " . $selected . " value='" . $row['id'] . "'>" . $row['lavel_name'] . "</option>";
                                     }
                                 }
                                 ?>
-                        </select>
+                            </select>
                         </div>
 
-                    <div>
-                        <?php if($permissions[0]['W']){?>
-                        <input name="btnAdd" type="submit" value="Add" class="btn btn-primary"/>
-                        <input name="btnUpdate" onclick="" type="submit" value="Update" class="btn btn-primary"/>
-                        <?php } else {
-                        ?>
-                        <input name="btnAdd" type="submit" value="Add" class="btn btn-primary" disabled/>
-                        <input name="btnUpdate" onclick="" type="submit" value="Update" class="btn btn-primary"disabled/>
-                            <?php
-                        }
-                        if($permissions[0]['D']){?>
-                        <input name="btnDelete" type="submit" value="Delete" class="btn btn-danger"/>
-                        <?php } else {
-                            ?>
-                            <input name="btnDelete" type="submit" value="Delete" class="btn btn-danger"/>
-                            <?php
-                        } ?>
-                        <input name="btnClear" type="reset" value="Clear" class="btn btn-default"/>
-                    </div>
-
-                            <div>
+                        <div>
+                            <?php if ($permissions[0]['W']) { ?>
+                                <input name="btnAdd" type="submit" value="Add" class="btn btn-primary"/>
+                                <input name="btnUpdate" onclick="" type="submit" value="Update"
+                                       class="btn btn-primary"/>
+                            <?php } else {
+                                ?>
+                                <input name="btnAdd" type="submit" value="Add" class="btn btn-primary" disabled/>
+                                <input name="btnUpdate" onclick="" type="submit" value="Update" class="btn btn-primary"
+                                       disabled/>
                                 <?php
-                                include_once 'dbconfig.php'; //Connect to database
-                                $query = "SELECT distinct p.UserLevel_tbl_id, ul.lavel_name
+                            }
+                            if ($permissions[0]['D']) {
+                                ?>
+                                <input name="btnDelete" type="submit" value="Delete" class="btn btn-danger"/>
+                            <?php } else {
+                                ?>
+                                <input name="btnDelete" type="submit" value="Delete" class="btn btn-danger"/>
+                                <?php
+                            } ?>
+                            <input name="btnClear" type="reset" value="Clear" class="btn btn-default"/>
+                        </div>
+
+                        <div>
+                            <?php
+                            include_once 'dbconfig.php'; //Connect to database
+                            $query = "SELECT distinct p.UserLevel_tbl_id, ul.lavel_name
                                 FROM  privileges_tbl p
                                 INNER JOIN userlevel_tbl ul ON p.UserLevel_tbl_id = ul.id";
-                                $result =getData($query);
-                                echo "<table width='100%'>"; // start a table tag in the HTML
-                                echo "<th>USER LEVEL</th><th>&nbsp;</th></tr>";
-                                while($row = mysqli_fetch_array($result)){   //Creates a loop to loop through results
-                                    echo "<tr><td>".$row['lavel_name']."</td><td><a href='userPrivileges.php?edit=".$row['UserLevel_tbl_id']."'>View</a></td></tr>";  //$row['index'] the index here is a field name
-                                }
-                                echo "</table>"; //Close the table in HTML
-                                connection_close(); //Make sure to close out the database connection
-                                ?>
-                            </div>
+                            $result = getData($query);
+                            echo "<table width='100%'>"; // start a table tag in the HTML
+                            echo "<th>USER LEVEL</th><th>&nbsp;</th></tr>";
+                            while ($row = mysqli_fetch_array($result)) {   //Creates a loop to loop through results
+                                echo "<tr><td>" . $row['lavel_name'] . "</td><td><a href='userPrivileges.php?edit=" . $row['UserLevel_tbl_id'] . "'>View</a></td></tr>";  //$row['index'] the index here is a field name
+                            }
+                            echo "</table>"; //Close the table in HTML
+                            connection_close(); //Make sure to close out the database connection
+                            ?>
+                        </div>
                     </div>
                     <div class="col-lg-6">
                         <table name="tbl_previ">
-                            <tr><th>Form Name</th><th>R</th><th>W</th><th>D</th></tr>
-                        <?php
-                        if(!is_null($row_edt)){
-                            $i = 0;
-                            foreach($row_edt as $row){
-                                ?>
-                                <tr>
-                                    <td>
-                                        <input type='text' readonly name='txtname[]' value='<?php echo $row[2];?>'>
-                                        <input type='hidden' readonly name='txtid[]' value='<?php echo $row[1];?>'>
-                                    </td>
-                                    <td><input type='checkbox' name='cbR<?php echo $i?>' <?php echo ($row[3]=='1')?'checked':'' ?> value='1'></td>
-                                    <td><input type='checkbox' name='cbW<?php echo $i?>' <?php echo ($row[4]=='1')?'checked':'' ?> value='1'></td>
-                                    <td><input type='checkbox' name='cbD<?php echo $i?>' <?php echo ($row[5]=='1')?'checked':'' ?> value='1'></td>
+                            <tr>
+                                <th>Form Name</th>
+                                <th>R</th>
+                                <th>W</th>
+                                <th>D</th>
+                            </tr>
+                            <?php
+                            if (!is_null($row_edt)) {
+                                $i = 0;
+                                foreach ($row_edt as $row) {
+                                    ?>
+                                    <tr>
+                                        <td>
+                                            <input type='text' readonly name='txtname[]' value='<?php echo $row[2]; ?>'>
+                                            <input type='hidden' readonly name='txtid[]' value='<?php echo $row[1]; ?>'>
+                                        </td>
+                                        <td><input type='checkbox'
+                                                   name='cbR<?php echo $i ?>' <?php echo ($row[3] == '1') ? 'checked' : '' ?>
+                                                   value='1'></td>
+                                        <td><input type='checkbox'
+                                                   name='cbW<?php echo $i ?>' <?php echo ($row[4] == '1') ? 'checked' : '' ?>
+                                                   value='1'></td>
+                                        <td><input type='checkbox'
+                                                   name='cbD<?php echo $i ?>' <?php echo ($row[5] == '1') ? 'checked' : '' ?>
+                                                   value='1'></td>
 
 
-                                </tr>
-                                <?php
-                                $i++;
+                                    </tr>
+                                    <?php
+                                    $i++;
+                                }
+                            } else {
+                                include_once 'dbconfig.php';
+                                $result = getData("SELECT FormID, `Name` FROM form_tbl");
+                                $rows = mysqli_fetch_all($result);
+                                $i = 0;
+                                foreach ($rows as $row) {
+                                    ?>
+                                    <tr>
+                                        <td>
+                                            <input type='text' readonly name='txtname[]' value='<?php echo $row[1]; ?>'>
+                                            <input type='hidden' readonly name='txtid[]' value='<?php echo $row[0]; ?>'>
+                                        </td>
+                                        <td><input type='checkbox'
+                                                   name='cbR<?php echo $i ?>' <?php echo ($cbr_ == '1') ? 'checked' : '' ?>
+                                                   value='1'></td>
+                                        <td><input type='checkbox'
+                                                   name='cbW<?php echo $i ?>' <?php echo ($cbw_ == '1') ? 'checked' : '' ?>
+                                                   value='1'></td>
+                                        <td><input type='checkbox'
+                                                   name='cbD<?php echo $i ?>' <?php echo ($cbd_ == '1') ? 'checked' : '' ?>
+                                                   value='1'></td>
+
+
+                                    </tr>
+                                    <?php
+                                    $i++;
+                                }
+                                connection_close(); //Make sure to close out the database connection
                             }
-                        }else{
-                            include_once 'dbconfig.php';
-                            $result = getData("SELECT FormID, `Name` FROM form_tbl");
-                            $rows = mysqli_fetch_all($result);
-                            $i = 0;
-                            foreach($rows as $row){
-                                ?>
-                                <tr>
-                                    <td>
-                                        <input type='text' readonly name='txtname[]' value='<?php echo $row[1];?>'>
-                                        <input type='hidden' readonly name='txtid[]' value='<?php echo $row[0];?>'>
-                                    </td>
-                                    <td><input type='checkbox' name='cbR<?php echo $i?>' <?php echo ($cbr_=='1')?'checked':'' ?> value='1'></td>
-                                    <td><input type='checkbox' name='cbW<?php echo $i?>' <?php echo ($cbw_=='1')?'checked':'' ?> value='1'></td>
-                                    <td><input type='checkbox' name='cbD<?php echo $i?>' <?php echo ($cbd_=='1')?'checked':'' ?> value='1'></td>
-
-
-                                </tr>
-                                <?php
-                                $i++;
-                            }
-                            connection_close(); //Make sure to close out the database connection
-                        }
-                        ?>
+                            ?>
                     </div>
-
-
                     <?php } else {
                         ?>
                         <h1>You Do Not Have Permissions To This Page...!</h1>
                         <?php
                     } ?>
                 </form>
-                </div>
-                <!-- /.row -->
-
-
-
-				
             </div>
-            <!-- /.container-fluid -->
-
         </div>
-        <!-- /#page-wrapper -->
-
     </div>
-    <!-- /#wrapper -->
-
+</div>
 <?php include_once './inc/footer.php'; ?>
-</body></html>
+</body>
+</html>
